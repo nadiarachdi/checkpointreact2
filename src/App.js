edf9db96dd0542aa9e7e7b190aee6d048c56047f
nadiarachdi <nadysonia@yahoo.fr>
2021-04-07 20:32:03 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
+import bizerte from '../src/bizerte.png';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='all'>
+    <div >
+      <h1 className="title red">Belles villes de Tunisie</h1>
+      <br />
+      <img  width={865} height={450} style={{border: 'solid 2px black',maxWidth: '100vw'}} src= {bizerte}/>
+      <br />
+      <img  width={865} height={450} style={{border: 'solid 2px black',maxWidth: '100vw'}} src="./kairouan.png" />
     </div>
+    
+    <iframe width={865} height={450}  style={{border: 'solid 2px black',maxWidth: '100vw'}} src="https://www.youtube.com/embed/hRNkRrwsuZI" title="YouTube video player" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen ></iframe>
+    
+  </div>
+
   );
 }
-
 export default App;
